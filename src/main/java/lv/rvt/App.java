3472ff6ad5e355f56d13badf153eb2bf37038050
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-09-06 08:17:54 +0000

    Uzdevums Nr 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,39 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
+    // {
+    //     // Make your first Hello World app!
+    //     System.out.println("Once upon a time");
+    //     System.out.println("there was");
+    //     System.out.println("a program");
+    // }
+    // {
+    //     int age = 17;
+    //     String name = "Reinis";
+    //     Boolean isonline = false;
+
+    //     System.out.println(age);
+    //     System.out.println(name);
+    //     System.out.println(isonline);
+    // }
+    // {
+    //     Scanner scanner = new Scanner(System.in);
+
+    //     String name = scanner.nextLine();
+
+    //     System.out.println("Sveiks " + name);
+    // }
     {
-        // Make your first Hello World app!
+        Scanner scanner =new Scanner(System.in);
 
+        String name = scanner.nextLine();
+        String lastname = scanner.nextLine();
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + lastname + ", grupa: " + group);
     }
-
 }

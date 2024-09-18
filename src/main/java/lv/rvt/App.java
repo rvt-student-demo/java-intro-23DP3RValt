0be6b2e894ff15ee44e6eb8a734994e93aa5c6dc
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-09-18 06:59:14 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -27,13 +27,72 @@ public class App
 
     //     System.out.println("Sveiks " + name);
     // }
-    {
-        Scanner scanner =new Scanner(System.in);
+    // {
+    //     Scanner scanner =new Scanner(System.in);
 
-        String name = scanner.nextLine();
-        String lastname = scanner.nextLine();
-        String group = scanner.nextLine();
+    //     String name = scanner.nextLine();
+    //     String lastname = scanner.nextLine();
+    //     String group = scanner.nextLine();
 
-        System.out.println("Students: " + name + " " + lastname + ", grupa: " + group);
-    }
+    //     System.out.println("Students: " + name + " " + lastname + ", grupa: " + group);
+    // }
+    // {
+    //     Scanner scanner = new Scanner(System.in);
+
+    //     System.out.println("Give a number:");
+    //     int value = Integer.valueOf(scanner.nextLine());
+    //     System.out.println("You gave the number " + value);
+    // }
+    // {
+    //         Scanner scanner = new Scanner(System.in);
+    
+    //         System.out.println("Give a number:");
+    //         double value = Double.valueOf(scanner.nextLine());
+    //         System.out.println("You gave the number " + value);
+    // }
+//     {
+//             Scanner scanner = new Scanner(System.in);
+    
+//             System.out.println("Give a boolean:");
+//             boolean value = Boolean.valueOf(scanner.nextLine());
+//             System.out.println("You wrote " + value);
+//     }
+
+    // {
+    //     Scanner scanner = new Scanner(System.in);
+
+    //     System.out.println("Give a number:");
+    //     int value1 = Integer.valueOf(scanner.nextLine());
+    //     int value2 = Integer.valueOf(scanner.nextLine());
+        
+    //     System.out.println(value1 + value2);
+    //     System.out.println(value1 * value2);
+
+    //     double dalisana = Double.valueOf(value1) / Double.valueOf(value2);
+    //     System.out.println(dalisana);
+
+    // }
+{
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Give the first number: ");
+    int value1 = Integer.valueOf(scanner.nextLine());
+
+    System.out.println("Give the second number: ");
+    int value2 = Integer.valueOf(scanner.nextLine());
+
+    int sum = value1 + value2;
+    System.out.println(value1 + "+" + value2 + "=" + sum);
+
+    int min = value1 - value2;
+    System.out.println(value1 + "-" + value2 + "=" + min);
+
+    int reiz = value1 * value2;
+    System.out.println(value1 + "*" + value2 + "=" + reiz);
+
+    double div = (double) value1 / value2;
+    System.out.println(value1 + "/" + value2 + "=" + div);
+
+}
+
 }

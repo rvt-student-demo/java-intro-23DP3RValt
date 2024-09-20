3cc948ec34540da2497aa27fca73f61b84abecf6
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-09-20 07:52:07 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -95,15 +95,46 @@ public class App
 
 // }
 
+// {
+//     Scanner scanner = new Scanner(System.in);
+
+//     System.out.println("Give speed: ");
+//     int speed = Integer.valueOf(scanner.nextLine());
+
+//     if (speed > 120) {
+//         System.out.println("Speeding ticket!");
+//     }
+
+// }
+
 {
     Scanner scanner = new Scanner(System.in);
+    System.out.println("Give points [0-100]:");
+    int points = Integer.valueOf(scanner.nextLine());
 
-    System.out.println("Give speed: ");
-    int speed = Integer.valueOf(scanner.nextLine());
-    
-    if (speed > 120) {
-        System.out.println("Speeding ticket!");
+    if (points < 0) {
+        System.out.println("impossible!");
     }
-
+    else if (points <= 49) {
+        System.out.println("failed");
+    }
+    else if (points <= 59) {
+        System.out.println("1");
+    }
+    else if (points <= 69) {
+        System.out.println("2");
+    }
+    else if (points <= 79) {
+        System.out.println("3");
+    }
+    else if (points <= 89) {
+        System.out.println("4");
+    }
+    else if (points <= 100) {
+        System.out.println("5");
+    }
+    else if (points > 100) {
+        System.out.println("incredible!");
+    }
 }
 }

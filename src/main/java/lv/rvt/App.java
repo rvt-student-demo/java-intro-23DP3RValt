729c7d0c8c8c215e5188c4bcf114c4fd35e63455
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-11-06 08:46:08 +0000

    Person name age

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,17 @@
 package lv.rvt;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
+// import java.util.ArrayList;
+// import java.util.Scanner;
+// import java.util.Random;
 
 public class App {
     public static void main( String[] args ) {
-        Account MattehewsAccount = new Account("Arto's account", 1000.00);
-        Account MyAccount = new Account("Arto's account in Switzerland", 0.00);
+        Person ada = new Person("Ada");
+        Person antti = new Person("Antti");
+        Person martin = new Person("Martin");
 
-        System.out.println("Initial state");
-        System.out.println(MattehewsAccount);
-        System.out.println(MyAccount);
-
-        MattehewsAccount.withdraw(100);
-        System.out.println("The balance of Arto's account is now: " + MattehewsAccount.balance());
-        MyAccount.deposit(100);
-        System.out.println("The balance of Arto's other account is now: " + MyAccount.balance());
-
-        System.out.println("End state");
-        System.out.println(MattehewsAccount);
-        System.out.println(MyAccount);
+        ada.printPerson();
+        antti.printPerson();
+        martin.printPerson();
     }
 }
---
+++ src/main/java/lv/rvt/Person.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+public class Person {
+    private String name;
+    private int age;
+
+
+    public Person(String initialName) {
+        this.name = initialName;
+        this.age = 0;
+    }
+
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
+}

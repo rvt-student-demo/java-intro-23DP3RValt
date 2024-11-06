7bb44fd1f11bb696395d6c2039cffe58f7cd690f
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-11-06 08:07:02 +0000

    Your first bank transfer

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,20 +6,20 @@ import java.util.Random;
 
 public class App {
     public static void main( String[] args ) {
-        Account artosAccount = new Account("Arto's account", 100.00);
-        Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
+        Account MattehewsAccount = new Account("Arto's account", 1000.00);
+        Account MyAccount = new Account("Arto's account in Switzerland", 0.00);
 
         System.out.println("Initial state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
+        System.out.println(MattehewsAccount);
+        System.out.println(MyAccount);
 
-        artosAccount.withdraw(20);
-        System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
-        artosSwissAccount.deposit(200);
-        System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
+        MattehewsAccount.withdraw(100);
+        System.out.println("The balance of Arto's account is now: " + MattehewsAccount.balance());
+        MyAccount.deposit(100);
+        System.out.println("The balance of Arto's other account is now: " + MyAccount.balance());
 
         System.out.println("End state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
+        System.out.println(MattehewsAccount);
+        System.out.println(MyAccount);
     }
 }

4ca90aaec1d36d4c619d47a1abbf3d9cc22bbfe9
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-11-13 08:48:08 +0000

    Statistics part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,6 +8,8 @@ public class App {
     public static void main( String[] args ) {
         Statistics statistics = new Statistics();
         Scanner scanner = new Scanner(System.in);
+        int sumEven = 0;
+        int sumOdd = 0; 
 
         while (true) {
             int newNumber = Integer.valueOf(scanner.nextLine());
@@ -16,11 +18,18 @@ public class App {
                 break;
             }
 
-            statistics.addNumber(newNumber);
+            if (newNumber % 2 == 0) {
+                sumEven += newNumber;
+            }
+            else {
+                sumOdd += newNumber;
+            }
         }
 
         System.out.println("Count: " + statistics.getCount());
         System.out.println("Sum: " + statistics.sum());
         System.out.println("Average: " + statistics.average());
+        System.out.println("Sum of even numbers: " + sumEven);
+        System.out.println("Sum of odd numbers: " + sumOdd);
     }
 }

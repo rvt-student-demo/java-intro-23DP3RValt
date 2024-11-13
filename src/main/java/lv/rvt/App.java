64c55d52535e7428fa88bf0b130f86463c3a5c73
23DP3RValt <166013545+23DP3RValt@users.noreply.github.com>
2024-11-13 08:33:06 +0000

    Satatistics part 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,26 @@
 package lv.rvt;
 
 // import java.util.ArrayList;
-// import java.util.Scanner;
+import java.util.Scanner;
 // import java.util.Random;
 
 public class App {
     public static void main( String[] args ) {
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int newNumber = Integer.valueOf(scanner.nextLine());
+
+            if (newNumber == -1) {
+                break;
+            }
+
+            statistics.addNumber(newNumber);
+        }
+
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 }
